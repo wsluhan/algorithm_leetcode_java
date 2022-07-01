33c8f3a08f806e83fc195cbea207bc0309df2949
wsluhan <lindaqiu1984@gmail.com>
2022-07-01 19:39:53 -0400

    Initial commit

---
+++ src/fcc_code_example_recursion/Fibonacci.java
@@ -0,0 +1,17 @@
+/*
+ * Code example from freeCodeCamp.org
+ * url = https://www.youtube.com/watch?v=IJDJ0kBx2LM
+ * */
+
+package fcc_code_example_recursion;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        long res = fib(10);
+        System.out.println(res);
+    }
+    public static long fib(long n) {
+        if ((n == 1) || (n == 0)) return n;
+        else return fib(n - 1) + fib(n - 2);  // always generate the left operand first
+    }
+}
